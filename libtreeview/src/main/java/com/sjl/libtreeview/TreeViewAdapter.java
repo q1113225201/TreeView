cbfa5bebd8be5d5a1c65d1e5e6daba1498093efd
林zero <1113225201@qq.com>
2018-01-14 17:42:54 +0800

    项目创建

---
+++ libtreeview/src/main/java/com/sjl/libtreeview/TreeViewAdapter.java
@@ -0,0 +1,11 @@
+package com.sjl.libtreeview;
+
+/**
+ * TreeViewAdapter
+ *
+ * @author 林zero
+ * @date 2018/1/14
+ */
+
+public class TreeViewAdapter {
+}
---
+++ libtreeview/src/main/java/com/sjl/libtreeview/bean/TreeNode.java
@@ -0,0 +1,77 @@
+package com.sjl.libtreeview.bean;
+
+import java.util.List;
+
+/**
+ * TreeNode
+ *
+ * @author 林zero
+ * @date 2018/1/14
+ */
+
+public class TreeNode<T> {
+    /**
+     * 当前节点值
+     */
+    private T value;
+    /**
+     * 父节点
+     */
+    private TreeNode parentNode;
+    /**
+     * 孩子节点列表
+     */
+    private List<TreeNode> childNodes;
+    /**
+     * 是否已展开
+     */
+    private boolean isExpanded;
+    /**
+     * 是否被选中
+     */
+    private boolean isSelected;
+
+    public TreeNode(T value) {
+        this.value = value;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    public void setValue(T value) {
+        this.value = value;
+    }
+
+    public TreeNode getParentNode() {
+        return parentNode;
+    }
+
+    public void setParentNode(TreeNode parentNode) {
+        this.parentNode = parentNode;
+    }
+
+    public List<TreeNode> getChildNodes() {
+        return childNodes;
+    }
+
+    public void setChildNodes(List<TreeNode> childNodes) {
+        this.childNodes = childNodes;
+    }
+
+    public boolean isExpanded() {
+        return isExpanded;
+    }
+
+    public void setExpanded(boolean expanded) {
+        isExpanded = expanded;
+    }
+
+    public boolean isSelected() {
+        return isSelected;
+    }
+
+    public void setSelected(boolean selected) {
+        isSelected = selected;
+    }
+}
